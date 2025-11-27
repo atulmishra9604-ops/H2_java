677e8b253ce90df7f1321e9efa42c700519a5d4a
rajsmishra25-beep <raj.s.mishra25@slrtce.in>
2025-11-27 09:20:28 +0530

    2nd commit of calculation

---
+++ H2_java/src/Com_2/Main2.java
@@ -0,0 +1,14 @@
+package Com_2;
+interface operation{
+	void add();
+	void sub();
+	void multi();
+	void div();
+	
+}
+public class Main2 {
+    public static void main(String[] args) {
+		System.out.println("calculation");
+	}
+
+}
