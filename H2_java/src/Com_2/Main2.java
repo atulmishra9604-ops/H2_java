e9de1e294625206b71ba8635f88fce24676a2ca2
rajsmishra25-beep <raj.s.mishra25@slrtce.in>
2025-11-27 09:26:46 +0530

    merge kar raha hu

--- H2_java/src/Com_2/Main2.java
+++ H2_java/src/Com_2/Main2.java
@@ -6,6 +6,33 @@ interface operation{
 	void div();
 	
 }
+class Cal implements operation{
+
+	@Override
+	public void add() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void sub() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void multi() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void div() {
+		// TODO Auto-generated method stub
+		
+	}
+	
+}
 public class Main2 {
     public static void main(String[] args) {
 		System.out.println("calculation");
